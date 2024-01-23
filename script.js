e9cee34158e6f58940da4d77339794dd05dec9c7
portellaluana <100139810+portellaluana@users.noreply.github.com>
2024-01-23 20:14:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ const input = document.getElementsByTagName("input");
 const inputNome = document.querySelector("#nome");
 const inputEmail = document.querySelector("#email");
 const inputCEP = document.querySelector("#cep");
-const buttonCadastrar = document.getElementById("botao-cadastrar");
+const botaoCadastrar = document.getElementById("botao-cadastrar");
+const botaoEnviar = document.getElementById("botao-enviar");
 
+const inputHomem = document.getElementById("homem");
+const inputMulher = document.getElementById("mulher");
+const inputCrianca = document.getElementById("criança");
+const inputBebida = document.getElementById("bebidas");
+
+const sectionUsuario = document.querySelector(".content-1");
+const sectionPessoas = document.querySelector(".pessoas-container");
 const dadosUsuarios = [];
 
-buttonCadastrar.addEventListener("click", function (e) {
+botaoCadastrar.addEventListener("click", function (e) {
   const nome = inputNome.value;
   const email = inputEmail.value;
   const cep = inputCEP.value;
@@ -23,8 +31,25 @@ buttonCadastrar.addEventListener("click", function (e) {
 
   let dadoUsuario = JSON.stringify(cadastro);
   dadosUsuarios.push("dadoUsuario", dadoUsuario);
-
   localStorage.setItem("usuários", dadosUsuarios);
+
+  sectionUsuario.style.display = "none";
+  sectionPessoas.style.display = "inline-flex";
+  sectionPessoas.style.flexDirection = "column";
+
+  e.preventDefault();
+});
+
+botaoEnviar.addEventListener("click", function (e) {
+  const homem = inputHomem.value;
+  const mulher = inputMulher.value;
+  const crianca = inputCrianca.value;
+  const bebida = inputBebida.value;
+
+  console.log("homem", homem);
+  console.log("mulher", mulher);
+  console.log("crianca", crianca);
+  console.log("bebida", bebida);
 
   e.preventDefault();
 });
