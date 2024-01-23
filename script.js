edb2175766850df8fd3c7b860e8ebb6dbccb3c46
portellaluana <luanaportella@gmail.com>
2024-01-22 23:07:48 -0300

    feat: first page

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const checkbox = document.getElementById("checkbox");
-let fontLight = Array.from(document.querySelectorAll('.font-dark'))
+const inputNome = document.querySelector("#nome");
+const inputEmail = document.querySelector("#email");
+const inputCEP = document.querySelector("#cep");
+const buttonCadastrar = document.getElementById("botao-cadastrar");
+const arr = [];
 
-checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
-  document.body.classList.toggle("font-light");
+buttonCadastrar.addEventListener("click", function (e) {
+  const nome = inputNome.value;
+  const email = inputEmail.value;
+  const cep = inputCEP.value;
+
+  const cadastro = {
+    nome,
+    email,
+    cep,
+  };
+
+
+  let novoArr = JSON.stringify(cadastro);
+  arr.push('novoArr',novoArr);
+
+  localStorage.setItem("meuarr", arr);
+
+  e.preventDefault();
 });
+
+// let fontLight = Array.from(document.querySelectorAll('.font-dark'))
+
+// checkbox.addEventListener("change", function(){
+//   document.body.classList.toggle("dark");
+//   document.body.classList.toggle("font-light");
+// });
